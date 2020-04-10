9f433bff5ad277979cad347911e3ea0db67fc9e9
alexispascual <ap.alexispascual@gmail.com>
2020-04-10 00:41:38 -0400

    Adding model evaluation function with confusion matrix. This is not tested. Hopefully it works

--- cnn-kmeans.py
+++ cnn-kmeans.py
@@ -1,8 +1,10 @@
 import os
 import tensorflow as tf
-tf.config.set_visible_devices([], 'GPU')
+# tf.config.set_visible_devices([], 'GPU')
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sn
+import pandas as pd
 
 from tensorflow import keras
 from sklearn.utils import shuffle
@@ -13,7 +15,7 @@ def CreateModel():
     keras.layers.Conv2D(32, (3,3), activation = 'relu', padding='valid', bias_initializer='glorot_uniform', input_shape=(28, 28, 1)),
     keras.layers.Conv2D(32, (3,3), activation = 'relu', padding='valid', bias_initializer='glorot_uniform'),
     keras.layers.Flatten(),
-    keras.layers.Dense(32, activation='tanh', bias_initializer='glorot_uniform'),
+    keras.layers.Dense(16, activation='relu', bias_initializer='glorot_uniform'),
     ])
 
     return model
@@ -38,6 +40,7 @@ def CalculateLoss(model, x, y, training):
     y_ = model(x, training=training)
 
     return loss_object(y_true=y, y_pred=y_)
+    # return tf.nn.l2_loss(y - y_)
 
 def CalculateGradients(model, inputs, targets):
     # Caclulate the gradients with respect to the loss
@@ -67,7 +70,7 @@ def RecalculateCentroids(centroids, feature_vectors, y_true):
     
     for count, centroid in enumerate(centroids):
         
-        keep = tf.reduce_any(tf.math.equal(y_true, centroid), axis=1)
+        keep = tf.reduce_all(tf.math.equal(y_true, centroid), axis=1)
         tf.print(tf.reduce_sum(tf.cast(keep, tf.float32)), "Samples in centroid", count)
         
 
@@ -77,10 +80,53 @@ def RecalculateCentroids(centroids, feature_vectors, y_true):
         
     tf.print("=======Done recalculating centroids!=======")
     return tf.stack(new_centroids)
-           
+
+def EvaluateModel(x_test, y_test, model, centroids):
+    
+    correct_predictions = 0
+    feature_vectors = model(x_test)
+    predictions = []
+    
+    for index, feature_vector in enumerate(feature_vectors):
+        l2_norm = tf.norm(feature_vector - centroids, axis = 1)
+        prediction = tf.math.argmin(l2_norm)
+        predictions.append(prediction)
+        
+        if prediction == y_test[index]:
+            correct_predictions += 1
+    
+    total_accuracy = correct_predictions/y_test.shape[0]
+    
+    print(total_accuracy)
+    
+    # Create confusion matrix
+    confusion_matrix = tf.math.confusion_matrix(y_test, tf.stack(predictions), num_classes = 10)
+    
+    # Plot Confusion Matrix
+    df_cm = pd.DataFrame(confusion_matrix, range(10), range(10))
+    plt.figure(figsize=(10,7))
+    sn.set(font_scale=1.4) # for label size
+    sn.heatmap(df_cm, annot=True, annot_kws={"size": 16}) # font size
+
+    plt.show()
+    
+def CreateFeatureVectors(model, x_train, batch_size):
+    # Split the dataset into batches to make it trainable on the GPU
+    x_dataset = tf.data.Dataset.from_tensor_slices(x_train)
+    x_dataset = x_dataset.batch(batch_size)
+    
+    feature_vectors = []
+    
+    for x in x_dataset:
+        feature_vectors.append(model(x))
+       
+    return tf.concat(feature_vectors, axis = 0)
     
 # %% Main
 if __name__ == "__main__":
+    
+    num_epochs = 100
+    batch_size = 32
     
     # Initialize Dataset
     
@@ -105,7 +151,7 @@ if __name__ == "__main__":
     x_train = tf.cast(x_train, tf.float32)
     x_test = tf.cast(x_test, tf.float32)
     # %% Extract feature vectors from the training set
-    feature_vectors  = model(x_train)
+    feature_vectors = CreateFeatureVectors(model, x_train, batch_size)   
     
     # %% Get one feature vector per class to initialize centroids
     centroids = InitializeCentroids(feature_vectors, y_train)
@@ -114,15 +160,12 @@ if __name__ == "__main__":
     y_true = AssignTargets(feature_vectors, centroids)
     
     # %% Adjust centroids (?)
-    # centroids = RecalculateCentroids(centroids, feature_vectors, y_true)
-    RecalculateCentroids(centroids, feature_vectors, y_true)
+    centroids = RecalculateCentroids(centroids, feature_vectors, y_true)
+    
     # %% Initialize Training parameters
     train_loss_results = []
     train_accuracy_results = []
-    
-    num_epochs = 100
-    batch_size = 16
-    
+       
     optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
     
     # %% Start training
@@ -153,19 +196,25 @@ if __name__ == "__main__":
         train_accuracy_results.append(epoch_mse.result())
           
         # Recalculate feature vectors
-        feature_vectors = model(x_train)
+        feature_vectors = CreateFeatureVectors(model, x_train, batch_size)   
+        
+        # Reassign y_true for each x_train
+        y_true = AssignTargets(feature_vectors, centroids)
         
         # Recalculate Centroids
         centroids = RecalculateCentroids(centroids, feature_vectors, y_true)
-
-        # Reassign y_true for each x_train
-        y_true = AssignTargets(feature_vectors, centroids)
         
         # Print results per epoch
         if epoch % 1 == 0:
           print("Epoch {:03d}: Loss: {:.6f}, MSE: {:.6f}".format(epoch,
                                                                       epoch_loss_avg.result(),
                                                                       epoch_mse.result()))
+    print("Done training! Saving Weights")          
+    model.save("./saved_model")
+    
+    # %% Evaluate Model
+    
+    EvaluateModel(x_test, y_test, model, centroids)
     
     
 
